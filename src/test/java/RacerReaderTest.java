729e0a7388bce7d52d52568dda81f28039c3bbe4
emrys <emriesy@gmail.com>
2021-07-27 12:48:25 +0200

    add test to RacerReader

--- src/test/java/RacerReaderTest.java
+++ src/test/java/RacerReaderTest.java
@@ -3,12 +3,14 @@ import com.foxminded.formula.RacersInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RacerReaderTest {
     private static final String ABR_PATH = "src/main/resources/abbreviations.txt";
     private static final String START_PATH = "src/main/resources/start.log";
     private static final String END_PATH = "src/main/resources/end.log";
+    private static final String WORNG_PATH = "src/main/resources/onion.txt";
+    private static final String ARG_EXCEPTION = "It is not a log file";
     private RacerReader reader;
     private RacersInfo racersInfo;
 
@@ -25,5 +27,13 @@ public class RacerReaderTest {
         reader.readFromFile(ABR_PATH, racersInfo);
         assertTrue(!racersInfo.getAbbreviations().isEmpty() && !racersInfo.getStart().isEmpty() &&
                 !racersInfo.getEnd().isEmpty());
+    }
+    @Test
+    void readWrongLog(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            reader.readFromFile(WORNG_PATH, racersInfo);
+        });
+        String actual = exception.getMessage();
+        assertEquals(ARG_EXCEPTION,actual);
     }
 }
